75c385896fb3b5f7fcd712e9f4da82531e531443
vale12345678910 <valery.sturm@gymburgdorf.ch>
2024-06-12 21:04:15 +0200

    small commit design

---
+++ main/student/grades/grades.js
@@ -0,0 +1,12 @@
+document.getElementById("Solve").addEventListener("click", function(){
+    window.location.href = '../solve/solve.html'
+  })
+  
+  
+  document.getElementById("Corrections").addEventListener("click", function(){
+    window.location.href = '../corrections/corrections.html'
+  })
+  
+  document.getElementById("CorrectionChange").addEventListener("click", function(){
+    window.location.href = '../correctionChange/correctionChange.html'
+  })
